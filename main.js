c9dab75a39238aacfda510735355bed34898b1df
adiyathereh <ad.kydy@gmail.com>
2018-08-13 14:43:21 +0200

    implemented game is over after 30 seconds

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+window.onload = function() {
+  document.getElementById("start-button").onclick = function() {
+    startGame();
+    document.getElementById("start-button").disabled = "disabled";
+  };
+
+  function startGame() {
+    intervalId = setInterval(updateCanvas, 1);
+    gameStart = Date.now();
+    // Math.floor(millis/1000)
+    // console.log(gameStart)
+  }
+};
 
 var World = function() {
   this.gravity = 1.007;
   this.ground = canvas.height / 2;
 };
-
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
 var bR1 = new Background(10, 0, canvas.width, canvas.height - 50);
 var bR2 = new Background(bR1.x - bR1.width, 0, bR1.width, bR1.height);
 var mario = new Hero(100, 100);
 var world = new World();
 var enemy = new Enemy(100);
 var intervalId;
+var gameStart,
+  timePassed = 0;
 var timeCounter = 0;
 var playerHorizontalMovementFactor = 0;
 
@@ -24,6 +38,13 @@ function updateCanvas() {
   drawBackground();
   drawHero();
   drawEnemy();
+
+  timePassed = Date.now() - gameStart;
+  console.log("seconds elapsed = " + Math.floor(timePassed / 1000));
+  if (Math.floor(timePassed / 1000) >= 30) {
+    gameEnd();
+    clearInterval(intervalId)
+  }
 }
 
 document.onkeydown = function(e) {
@@ -48,4 +69,8 @@ document.onkeyup = function(e) {
   }
 };
 
-intervalId = setInterval(updateCanvas, 10);
+function gameEnd() {
+  console.log("game ended");
+}
+
+// intervalId = setInterval(updateCanvas, 10);
